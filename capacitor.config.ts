384e21e868b68de8a18309d84dd307cee682eb55
marghherita <margherita.grasso3@gmail.com>
2025-06-15 22:37:28 +0200

    first commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'dietApp',
+  webDir: 'dist'
+};
+
+export default config;
